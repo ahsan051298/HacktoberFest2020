8909888bbf425b5a782406f75a66c5996840075d
Gitanjali-1234 <72544109+Gitanjali-1234@users.noreply.github.com>
2020-10-28 16:05:12 +0530

    Create Gitanjali.java

---
+++ Gitanjali.java
@@ -0,0 +1,16 @@
+class Fibonacciexample1{
+publice static void main(string args[])
+{
+int n1=0,n2=2,n3,i,count=10;
+system.out.print(n1+" "+n2);
+
+for(i=2;i<count;++i)
+{
+n3=n1+n2;
+system.out.print(" "+n3);
+m1=n2;
+n2=n3;
+}
+
+}
+}
